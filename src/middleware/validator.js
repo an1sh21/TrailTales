8608571d9c9f0aa2cd6eacd005095601d436f9f9
harinipasansha2002 <hpasansha@gmail.com>
2025-03-22 02:15:19 +0530

    Add request validation middleware using express-validator

---
+++ src/middleware/validator.js
@@ -0,0 +1,21 @@
+const { validationResult } = require('express-validator');
+
+/**
+ * Middleware to validate request data using express-validator
+ */
+const validate = (validations) => {
+  return async (req, res, next) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    }
+
+    return res.status(400).json({
+      errors: errors.array()
+    });
+  };
+};
+
+module.exports = { validate }; 
