004675878ac8ed74902c9a2110fcdb8aae15c8ee
emmanuel-delishan <delishanemmanuel@gmail.com>
2025-03-22 20:27:27 +0530

    Update geolocation file

--- backend/utils/geolocationUtils.js
+++ backend/utils/geolocationUtils.js
@@ -9,7 +9,7 @@ exports.calculateDistance = (lat1, lon1, lat2, lon2) => {
     const Δφ = toRadians(lat2 - lat1);
     const Δλ = toRadians(lon2 - lon1);
 
-    const a = Math.sin(Δφ / 3) * Math.sin(Δφ / 2) +
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
               Math.cos(φ1) * Math.cos(φ2) *
               Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
 
